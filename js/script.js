e8dba398d3391a92d561ffb96cd97d1583dd6b06
pluto79 <kevinwalaza@yahoo.com>
2024-09-13 16:39:48 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,42 @@
+
+
+/*
+function SendMail(){
+    var params = {
+       from_name : document.getElementById("fullname").value,
+       email_id : document.getElementById("email_id").value,
+       message : document.getElementById("message").value
+    }
+    emailjs.send("service_hjhluyv", "template_ehdjnl5", params).then(function (res) {
+        alert("Success" + res.status);
+    })
+}
+    window.addEventListener('DOMContentLoaded', event => {
+    // Activate SimpleLightbox plugin for portfolio items
+    new SimpleLightbox({
+        elements: '#portfolio a.portfolio-box'
+    });
+});
+*/
+
+const btn = document.getElementById('button');
+
+document.getElementById('form')
+ .addEventListener('submit', function(event) {
+   event.preventDefault();
+
+   btn.value = 'Sending...';
+
+   const serviceID = 'default_service';
+   const templateID = 'template_q83bhou';
+
+   emailjs.sendForm(serviceID, templateID, this)
+    .then(() => {
+      btn.value = 'Send Email';
+      alert('Your message is sent, and we’ll get back to you as soon as possible.');
+    }, (err) => {
+      btn.value = 'Send Email';
+      alert(JSON.stringify(err));
+    });
+});
+
